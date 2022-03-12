98cd2f9ef69b3d2a443357048ecfa89c394a3d4b
Jakub Szachewicz <jszachewicz@vewd.com>
2022-03-12 14:25:19 +0100

    port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const tfjs = require("@tensorflow/tfjs-node")
 const path = require("path")
 const app = express()
 
-let port = process.env.port || 3200
+let port = process.env.PORT || 3000
 
 app.get("/", async (req, res) => {
     const model = await tfjs.loadLayersModel("file://" + path.join( __dirname, "models/model.json"))
