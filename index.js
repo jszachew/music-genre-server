5b34754519cc067a20852af560d86181e909cee8
Jakub Szachewicz <jszachewicz@vewd.com>
2022-03-12 14:18:07 +0100

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const tfjs = require("@tensorflow/tfjs-node")
+const path = require("path")
+const app = express()
+
+let port = process.env.port || 3200
+
+app.get("/", async (req, res) => {
+    const model = await tfjs.loadLayersModel("file://" + path.join( __dirname, "models/model.json"))
+    model.summary()
+    res.send("Hello world")
+})
+
+app.listen(port, () =>{
+    console.log(`running server on port ${port}`)
+})
