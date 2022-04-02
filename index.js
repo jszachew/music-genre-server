4fd14328c84f7966710c155001bd328d7490e1cc
Jakub Szachewicz <jszachewicz@vewd.com>
2022-04-02 10:01:36 +0200

    server ended

--- index.js
+++ index.js
@@ -2,15 +2,96 @@ const express = require("express")
 const tfjs = require("@tensorflow/tfjs-node")
 const path = require("path")
 const app = express()
-
+//import {wordsArray} from "./utils/words.js"
+const wordsArray = require("./utils/words.js")
+const song = require("./utils/example_song")
+const bodyParser = require('body-parser');
 let port = process.env.PORT || 3000
 
+const processText = (text, wordsArray) => {
+    const preprocessedText = text.toLowerCase().replace(/,/g,"").replace(/\n/g, " ").replace("!","").replace("(","").replace(")","").replace(",","")
+    textArray = preprocessedText.split(" ")
+    let counter = 0
+    let map = {}
+    textArray.forEach(item => {
+        if(wordsArray.includes(item)){
+            counter = counter + 1;
+            if(map[item]){
+                map[item] = map[item] + 1
+            } else {
+                map[item] = 1
+            }
+        }
+    })
+    let tensor = new Array(5000).fill(0);
+
+    for (const [key, value] of Object.entries(map)){
+        let index = wordsArray.indexOf(key)
+        tensor[index] = value
+    }
+    return tensor
+}
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.raw());
+app.use(express.json())
 app.get("/", async (req, res) => {
     const model = await tfjs.loadLayersModel("file://" + path.join( __dirname, "models/model.json"))
-    model.summary()
-    res.send("Hello world")
+    const tensor = tfjs.tensor([processText(song, wordsArray)])
+    const result = model.predict(tensor)
+    const resutlArr = result.dataSync()
+    const genres = ["Blues",
+        "Country",
+        "Electronic",
+        "Folk",
+        "International",
+        "Jazz",
+        "Latin",
+        "NewAge",
+        "PopRock",
+        "Rap",
+        "Reggae",
+        "RnB",
+        "Vocal"]
+    const resultObj = {}
+    resutlArr.forEach((item, idx) => {
+        resultObj[genres[idx]] = item
+        }
+    )
+    console.log(resultObj)
+    res.json(resultObj)
 })
+
+app.post('/', async(req, res) => {
+    const model = await tfjs.loadLayersModel("file://" + path.join( __dirname, "models/model.json"))
+    const text = req.body.text
+    console.log(text)
+    const tensor = tfjs.tensor([processText(text, wordsArray)])
+    const result = model.predict(tensor)
+    const resutlArr = result.dataSync()
+    const genres = ["Blues",
+        "Country",
+        "Electronic",
+        "Folk",
+        "International",
+        "Jazz",
+        "Latin",
+        "NewAge",
+        "PopRock",
+        "Rap",
+        "Reggae",
+        "RnB",
+        "Vocal"]
+    const resultObj = {}
+    resutlArr.forEach((item, idx) => {
+            resultObj[genres[idx]] = item
+        }
+    )
+    console.log(resultObj)
+    res.json(resultObj)
+});
 
 app.listen(port, () =>{
     console.log(`running server on port ${port}`)
+    processText(song, wordsArray)
+    console.log(wordsArray.length)
 })
---
+++ utils/example_song.js
@@ -0,0 +1,106 @@
+module.exports =
+    "Obie Trice, real name no gimmicks\n" +
+    "Two trailer park girls go round the outside\n" +
+    "Round the outside, round the outside\n" +
+    "Two trailer park girls go round the outside\n" +
+    "Round the outside, round the outside\n" +
+    "Guess who's back\n" +
+    "Back again\n" +
+    "Shady's back\n" +
+    "Tell a friend\n" +
+    "Guess who's back, guess who's back\n" +
+    "Guess who's back, guess who's back\n" +
+    "Guess who's back, guess who's back\n" +
+    "Guess who's back\n" +
+    "I've created a monster\n" +
+    "'Cause nobody wants to see Marshall no more\n" +
+    "They want Shady, I'm chopped liver\n" +
+    "Well if you want Shady, this is what I'll give you\n" +
+    "A little bit of weed mixed with some hard liquor\n" +
+    "Some vodka that'll jump start my heart quicker\n" +
+    "Than a shock when I get shocked at the hospital\n" +
+    "By the doctor when I'm not co-operating\n" +
+    "When I'm rockin' the table while he's operating\n" +
+    "You waited this long, now stop debating\n" +
+    "'Cause I'm back, I'm on the rag and ovulating\n" +
+    "I know that you got a job, Ms. Cheney\n" +
+    "But your husband's heart problem's complicating\n" +
+    "So the FCC won't let me be\n" +
+    "Or let me be me, so let me see\n" +
+    "They try to shut me down on MTV\n" +
+    "But it feels so empty, without me\n" +
+    "So, come on and dip, bum on your lips\n" +
+    "Fuck that, cum on your lips, and some on your tits\n" +
+    "And get ready, 'cause this shit's about to get heavy\n" +
+    "I just settled all my lawsuits, fuck you Debbie!\n" +
+    "Now this looks like a job for me\n" +
+    "So everybody, just follow me\n" +
+    "'Cause we need a little, controversy\n" +
+    "'Cause it feels so empty, without me\n" +
+    "I said \"This looks like a job for me\"\n" +
+    "So everybody, just follow me\n" +
+    "'Cause we need a little, controversy\n" +
+    "'Cause it feels so empty, without me\n" +
+    "Little Hellions, kids feelin' rebellious\n" +
+    "Embarrassed their parents still listen to Elvis\n" +
+    "They start feelin' like prisoners helpless\n" +
+    "'Til someone comes along on a mission and yells, bitch!\n" +
+    "A visionary, vision of scary\n" +
+    "Could start a revolution, pollutin' the airwaves\n" +
+    "A rebel, so just let me revel and bask\n" +
+    "In the fact that I got everyone kissin' my ass\n" +
+    "And it's a disaster, such a catastrophe\n" +
+    "For you to see so damn much of my ass\n" +
+    "You asked for me?\n" +
+    "Well I'm back\n" +
+    "Na-na-na-na-na-na-na-na-na-na\n" +
+    "Fix your bent antenna tune it in and then I'm gonna\n" +
+    "Enter in, endin' up under your skin like a splinter\n" +
+    "The center of attention, back for the winter\n" +
+    "I'm interesting, the best thing since wrestling\n" +
+    "Infesting in your kid's ears and nesting\n" +
+    "Testing, attention please\n" +
+    "Feel the tension, soon as someone mentions me\n" +
+    "Here's my ten cents, my two cents is free\n" +
+    "A nuisance, who sent? You sent for me?\n" +
+    "Now this looks like a job for me\n" +
+    "So everybody, just follow me\n" +
+    "'Cause we need a little, controversy\n" +
+    "'Cause it feels so empty, without me\n" +
+    "I said, \"This looks like a job for me\"\n" +
+    "So everybody, just follow me\n" +
+    "'Cause we need a little, controversy\n" +
+    "'Cause it feels so empty, without me\n" +
+    "A-tisket a-tasket, I go tit for tat with\n" +
+    "Anybody who's talkin' this shit, that shit\n" +
+    "Chris Kirk Patrick, you can get your ass kicked\n" +
+    "Worse than them little Limp Bizkit bastards\n" +
+    "And Moby? You can get stomped by Obie\n" +
+    "You thirty-six year old baldheaded fag, blow me\n" +
+    "You don't know me, you're too old, let go\n" +
+    "It's over, nobody listen to techno\n" +
+    "Now let's go, just gimme the signal\n" +
+    "I'll be there with a whole list full of new insults\n" +
+    "I been dope, suspenseful with a pencil\n" +
+    "Ever since Prince turned himself into a symbol\n" +
+    "But sometimes the shit just seems\n" +
+    "Everybody only wants to discuss me\n" +
+    "So this must mean I'm disgusting\n" +
+    "But it's just me, I'm just obscene\n" +
+    "No I'm not the first king of controversy\n" +
+    "I am the worst thing since Elvis Presley\n" +
+    "To do black music so selfishly\n" +
+    "And used it to get myself wealthy\n" +
+    "There's a concept that works\n" +
+    "Twenty million other white rappers emerge\n" +
+    "But no matter how many fish in the sea\n" +
+    "It'll be so empty, without me\n" +
+    "Now this looks like a job for me\n" +
+    "So everybody, just follow me\n" +
+    "'Cause we need a little, controversy\n" +
+    "'Cause it feels so empty, without me\n" +
+    "I said \"This looks like a job for me\"\n" +
+    "So everybody, just follow me\n" +
+    "'Cause we need a little, controversy\n" +
+    "'Cause it feels so empty, without me\n" +
+    "Kids!\n"
